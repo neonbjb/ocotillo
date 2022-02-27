c9ddbadb12dde4c0bf8a735bfe1ce1bf4c91af7e
James Betker <jbetker@gmail.com>
2022-02-26 20:21:34 -0700

    Update distribution

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from api import Transcriber
+from utils import load_wav_to_torch, load_audio
---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ocotillo",
+    packages=["."],
+    version="1.0.1",
+    author="James",
+    author_email="james@adamant.ai",
+    description="A simple & fast speech transcription toolkit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/neonbjb/ocotillo",
+    project_urls={},
+    install_requires=[
+        'tqdm',
+        'scipy',
+        'torch>=1.8',
+        'torchaudio>0.9',
+        'audio2numpy',
+        'transformers',
+        'tokenizers',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    download_url = 'https://github.com/neonbjb/ocotillo/archive/refs/tags/1.0.0.tar.gz',
+    python_requires=">=3.6",
+)
